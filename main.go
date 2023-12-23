f9dc413cc51c736baeeaa1a93d675ea58bd83430
AkashKanteti <akashkanteti@gmail.com>
2023-12-23 19:24:39 +0530

    added word counter

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	wc := flag.NewFlagSet("wc", flag.ExitOnError)
 	byteCounter := wc.Bool("c", false, "to set byte counter")
 	lineCounter := wc.Bool("l", false, "to set line countet")
+	wordCounter := wc.Bool("w", false, "to set word countet")
+
 	switch os.Args[1] {
 	case "wc":
 		wc.Parse(os.Args[2:])
@@ -25,21 +27,37 @@ func main() {
 		cnt, err := os.ReadFile(fileName)
 		if err != nil {
 			fmt.Printf("%v", err)
+			os.Exit(1)
 		}
 		if *byteCounter {
 			fmt.Printf("%v %v", len(cnt), fileName)
 		} else if *lineCounter {
-			lines := splitter(cnt)
+			lines := splitter(cnt, "\n")
 			fmt.Printf("%v %v", len(lines), fileName)
+		} else if *wordCounter {
+			var count int
+
+			lines := splitter(cnt, "\n")
+
+			for _, line := range lines {
+				line = strings.TrimSpace(line)
+				arr := strings.Fields(line)
+				count = count + len(arr)
+			}
+
+			fmt.Printf("%v %v", count, fileName)
 		} else {
 			fmt.Printf("No option choosen")
 		}
 	}
 }
 
-func splitter(text []byte) []string {
+func splitter(text []byte, sep string) []string {
 	str := string(text)
-	str = strings.Trim(str, " \n")
-	arr := strings.Split(str, "\n")
+	arr := strings.FieldsFunc(str, fun)
 	return arr
+}
+
+func fun(r rune) bool {
+	return r == '\n'
 }
